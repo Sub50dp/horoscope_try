b28addb8d84293266e0c57be6155acfda2275544
day26 <day26@i.ua>
2023-05-04 16:08:08 +0300

    start

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("type/", views.type_zodiac),
-    path("type/<sign_type>", views.types_zodiacs, name="type-name"),
+    path("type/<str:sign_type>", views.types_zodiacs, name="type-name"),
     path("<int:sign_zodiac>/", views.get_info_zodiac_int),
     path("<str:sign_zodiac>/", views.get_info_zodiac, name="horoscope-name"),
+    path("<int:month>/<int:day>", views.get_zodiac_date)
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -3,18 +3,18 @@ from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
 zodiac_dict = {
-    'aries': 'Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).',
-    'taurus': 'Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).',
-    'gemini': 'Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).',
-    'cancer': 'Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).',
-    'leo': ' Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).',
-    'virgo': 'Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).',
-    'libra': 'Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).',
-    'scorpio': 'Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).',
-    'sagittarius': 'Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).',
-    'capricorn': 'Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).',
-    'aquarius': 'Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).',
-    'pisces': 'Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).',
+    'aries': ('Овен - первый знак зодиака, планета Марс (с 21 марта по 20 апреля).', {3: range(21, 32), 4: range(1, 21)}),
+    'taurus': ('Телец - второй знак зодиака, планета Венера (с 21 апреля по 21 мая).', {4: range(21, 31), 5: range(1, 22)}),
+    'gemini': ('Близнецы - третий знак зодиака, планета Меркурий (с 22 мая по 21 июня).', {5: range(22, 32), 6: range(1, 22)}),
+    'cancer': ('Рак - четвёртый знак зодиака, Луна (с 22 июня по 22 июля).', {6: range(22, 31), 7: range(1, 23)}),
+    'leo': ('Лев - пятый знак зодиака, солнце (с 23 июля по 21 августа).', {7: range(23, 32), 8: range(1, 22)}),
+    'virgo': ('Дева - шестой знак зодиака, планета Меркурий (с 22 августа по 23 сентября).', {8: range(22, 32), 9: range(1, 24)}),
+    'libra': ('Весы - седьмой знак зодиака, планета Венера (с 24 сентября по 23 октября).', {9: range(24, 31), 10: range(1, 24)}),
+    'scorpio': ('Скорпион - восьмой знак зодиака, планета Марс (с 24 октября по 22 ноября).', {10: range(24, 32), 11: range(1, 23)}),
+    'sagittarius': ('Стрелец - девятый знак зодиака, планета Юпитер (с 23 ноября по 22 декабря).', {11: range(23, 31), 12: range(1, 23)}),
+    'capricorn': ('Козерог - десятый знак зодиака, планета Сатурн (с 23 декабря по 20 января).', {12: range(23, 32), 1: range(1, 21)}),
+    'aquarius': ('Водолей - одиннадцатый знак зодиака, планеты Уран и Сатурн (с 21 января по 19 февраля).', {1: range(21, 31), 2: range(1, 20)}),
+    'pisces': ('Рыбы - двенадцатый знак зодиака, планеты Юпитер (с 20 февраля по 20 марта).', {2: range(20, 29), 3: range(1, 21)}),
 }
 
 
@@ -22,11 +22,17 @@ def index(request):
     name_zodiac = list(zodiac_dict)
     li_zodiac = ""
     for sign in name_zodiac:
-        redirect_path = reverse("horoscope-name", args=[sign])  # адрес строки для перехода по ссылке
-        li_zodiac += f"<li> <a href='{redirect_path}'>{sign.title()}</a> </li>"
+        redirect_path = reverse("horoscope-name", args=[sign[0]])  # адрес строки для перехода по ссылке
+        li_zodiac += f"<li> <a href='{redirect_path}'>{sign[0].title()}</a> </li>"
     li_zodiac += '<li style="font-size:30px; color:red"><a href="/horoscope/type">Type</a></li>'
     return HttpResponse(f"<ul>{li_zodiac}</ul>")
 
+def get_zodiac_date(request, month, day):
+    for key, value in zodiac_dict.items():
+        redirect_path = reverse("horoscope-name", args=[key])
+        if month in value[1] and day in value[1].get(month):
+            return HttpResponseRedirect(redirect_path)
+    return HttpResponseNotFound('Знак зодиака не найден')
 
 def type_zodiac(request):
     type_li = ["fire", "earth", "air", "water"]
@@ -38,7 +44,7 @@ def type_zodiac(request):
 
 
 def types_zodiacs(request, sign_type):
-    type_dict_zodiac = {"air": ["gemini", "libras", "aquarius"],
+    type_dict_zodiac = {"air": ["gemini", "libra", "aquarius"],
                         "fire": ["aries", "leo", "sagittarius"],
                         "water": ["pisces", "cancer", "scorpio"],
                         "earth": ["capricorn", "virgo", "taurus"]}
@@ -50,9 +56,11 @@ def types_zodiacs(request, sign_type):
         return HttpResponse(f"<ul>{li_zodiac_for_type}</ul>")
 
 
-
 def get_info_zodiac(request, sign_zodiac: str):
-    return HttpResponse(zodiac_dict.get(sign_zodiac, HttpResponseNotFound(f"Неизвестный знак зодиака - {sign_zodiac}")))
+    if sign_zodiac in zodiac_dict:
+        return HttpResponse(zodiac_dict.get(sign_zodiac)[0])
+    else:
+        return HttpResponseNotFound(f"Неизвестный знак зодиака - {sign_zodiac}")
 
 
 def get_info_zodiac_int(request, sign_zodiac: int):
